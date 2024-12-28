00718dd7aef4eb92c42c7cc2920ae73b28dcacc3
Anand Radhakrishnan <aradhakr34@dt-login01.delta.ncsa.illinois.edu>
2024-12-28 01:57:28 -0600

    Delta Mako

--- examples/3D_TaylorGreenVortex/case.py
+++ examples/3D_TaylorGreenVortex/case.py
@@ -3,7 +3,7 @@
 import math
 import json
 
-N = 256 
+N = 999
 
 Re = 1600
 L = 1
@@ -13,7 +13,7 @@ C0 = math.sqrt(1.4*P0)
 V0 = 0.1*C0
 mu = V0*L/Re
 
-cfl = 0.5
+cfl = 0.3
 dx = 2*math.pi*L/(N+1)
 
 dt = cfl * dx / (C0)
@@ -22,8 +22,6 @@ tC = L/V0
 tEnd = 20*tC
 
 Nt = int(tEnd/dt)
-
-
 # Configuring case dictionary
 print(json.dumps({
     # Logistics ================================================
@@ -42,9 +40,10 @@ print(json.dumps({
     'p'                            : N,
     'cyl_coord'                    : 'F',
     'dt'                           : dt,
-    't_step_start'                 : 13529,
+    't_step_start'                 : 0,
     't_step_stop'                  : Nt,
-    't_step_save'                  : int(Nt/100),
+    't_step_save'                  : int(int(Nt/100)),    
+    
     # ==========================================================
 
     # Simulation Algorithm Parameters ==========================
@@ -52,13 +51,14 @@ print(json.dumps({
     'model_eqns'                   : 2,
     'alt_soundspeed'               : 'F',
     'num_fluids'                   : 1,
-    'mixture_err'                  : 'T',
+    'mixture_err'                  : 'F',
+    'igr'                          : 'T',
     'time_stepper'                 : 3,
     'weno_order'                   : 5,
     'weno_eps'                     : 1.0E-16,
     'weno_Re_flux'                 : 'F',
     'weno_avg'                     : 'F',
-    'mapped_weno'                  : 'T',
+    'mapped_weno'                  : 'F',
     'riemann_solver'               : 2,
     'wave_speeds'                  : 1,
     'avg_state'			           : 2,
@@ -71,15 +71,16 @@ print(json.dumps({
     # ==========================================================
 
     # Formatted Database Files Structure Parameters ============
-    'format'                       : 1,
+    'format'                       : 2,
     'precision'                    : 2,
-    # 'prim_vars_wrt'                :'T',
+    'prim_vars_wrt'                :'T',
     'omega_wrt(1)'                 :'T',
     'omega_wrt(2)'                 :'T',
     'omega_wrt(3)'                 :'T',
-    'qm_wrt'                       :'T',
+    'E_wrt'                        :'T',
     'fd_order'                     : 4,
     'parallel_io'                  :'T',
+    'rdma_mpi'                     :'F',
     # ==========================================================
 
     # I will use 1 for WATER properties, and 2 for AIR properties
@@ -91,10 +92,10 @@ print(json.dumps({
     'patch_icpp(1)%length_x'       : 2*math.pi*L,
     'patch_icpp(1)%length_y'       : 2*math.pi*L,
     'patch_icpp(1)%length_z'       : 2*math.pi*L,
-    'patch_icpp(1)%vel(1)'         : f"{V0}*sin(x/{L})*cos(y/{L})*sin(z/{L})",
-    'patch_icpp(1)%vel(2)'         : f"-{V0}*cos(x/{L})*sin(y/{L})*sin(z/{L})",
+    'patch_icpp(1)%vel(1)'         : 1,#f"{V0}*sin(x/{L})*cos(y/{L})*sin(z/{L})",
+    'patch_icpp(1)%vel(2)'         : 1,#f"-{V0}*cos(x/{L})*sin(y/{L})*sin(z/{L})",
     'patch_icpp(1)%vel(3)'         : 0,
-    'patch_icpp(1)%pres'           : f"{P0} + ({rho0}*{V0}**2/16)*(cos(2*x/{L}) + cos(2*y/{L}))*(cos(2*z/{L}) + 2)",
+    'patch_icpp(1)%pres'           : 1,#f"{P0} + ({rho0}*{V0}**2/16)*(cos(2*x/{L}) + cos(2*y/{L}))*(cos(2*z/{L}) + 2)",
     'patch_icpp(1)%alpha_rho(1)'   : 1,
     'patch_icpp(1)%alpha(1)'       : 1,
     # ==========================================================
@@ -107,3 +108,4 @@ print(json.dumps({
 }))
 
 # ==============================================================================
+
