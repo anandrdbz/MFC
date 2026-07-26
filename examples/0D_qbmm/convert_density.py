d5f4d8eb335fd24146653c240618d7b3fe80a7aa
Temp <temp@Temps-MacBook-Air.local>
2026-07-26 02:34:24 -0400

    final changes

---
+++ examples/0D_qbmm/convert_density.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+import numpy as np
+from scipy.stats import gaussian_kde
+
+
+base = Path(".")
+
+# Each file is assumed to contain 4 columns, one for each simulation time.
+rad = np.loadtxt(base / "Rad_mc.csv", delimiter=",")
+rdot = np.loadtxt(base / "Rdot_mc.csv", delimiter=",")
+
+# Grid used for the PGFPlots contour data.
+x = np.linspace(0.6, 1.4, 220)
+y = np.linspace(-0.6, 0.6, 220)
+
+X, Y = np.meshgrid(x, y)
+
+query_points = np.vstack([
+    X.ravel(),
+    Y.ravel(),
+])
+
+densities = []
+
+for i in range(4):
+    samples = np.vstack([
+        rad[:, i],
+        rdot[:, i],
+    ])
+
+    kde = gaussian_kde(samples)
+
+    density = kde(query_points).reshape(X.shape)
+
+    densities.append(density)
+
+# Use one common normalization across all four panels.
+density_max = max(
+    density.max()
+    for density in densities
+)
+
+for i, density in enumerate(densities, start=1):
+    density_normalized = density / density_max
+
+    # Prevent zero values on the logarithmic color scale.
+    density_normalized = np.maximum(
+        density_normalized,
+        1.0e-4,
+    )
+
+    output_file = base / f"density{i}.csv"
+
+    with output_file.open("w") as file:
+        file.write("R,Rdot,density\n")
+
+        for iy in range(len(y)):
+            for ix in range(len(x)):
+                file.write(
+                    f"{X[iy, ix]:.12e},"
+                    f"{Y[iy, ix]:.12e},"
+                    f"{density_normalized[iy, ix]:.12e}\n"
+                )
+
+            # Blank line separates adjacent grid rows for PGFPlots.
+            file.write("\n")
+
+    print(f"Created {output_file}")
--- examples/0D_qbmm/plot.py
+++ examples/0D_qbmm/plot.py
@@ -1,6 +1,94 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+SGM_EPS = 10**-16
+
+def s_hyqmom(fmom):
+    """
+    Symmetric two-node quadrature based on moments [m0, m1, m2].
+    """
+    fmom = np.asarray(fmom, dtype=float)
+
+    bu = fmom[1] / fmom[0]
+    d2 = fmom[2] / fmom[0]
+    c2 = max(d2 - bu**2, SGM_EPS)
+
+    frho = np.array([
+        fmom[0] / 2.0,
+        fmom[0] / 2.0,
+    ])
+
+    fup = np.array([
+        bu - np.sqrt(c2),
+        bu + np.sqrt(c2),
+    ])
+
+    return frho, fup
+
+
+def compute_2d_quadrature(momin):
+    """
+    Construct a four-node conditional quadrature from the six moments
+    [m00, m10, m01, m20, m11, m02].
+    """
+    momin = np.asarray(momin, dtype=float)
+
+    m00, m10, m01, m20, m11, m02 = momin
+
+    bu = m10 / m00
+    bv = m01 / m00
+
+    d20 = m20 / m00
+    d11 = m11 / m00
+    d02 = m02 / m00
+
+    c20 = d20 - bu**2
+    c11 = d11 - bu * bv
+    c02 = d02 - bv**2
+
+    # X quadrature
+    M1 = np.array([1.0, 0.0, c20])
+    myrho, up = s_hyqmom(M1)
+
+    # Conditional mean in Y
+    Vf = c11 * up / c20
+
+    # Conditional variance in Y
+    mu2 = max(
+        SGM_EPS,
+        c02 - np.sum(myrho * Vf**2),
+    )
+
+    # Y quadrature
+    M3 = np.array([1.0, 0.0, mu2])
+    myrho3, up3 = s_hyqmom(M3)
+
+    # Weights
+    wght = m00 * np.array([
+        myrho[0] * myrho3[0],
+        myrho[0] * myrho3[1],
+        myrho[1] * myrho3[0],
+        myrho[1] * myrho3[1],
+    ])
+
+    # Abscissas
+    abscX = bu + np.array([
+        up[0],
+        up[0],
+        up[1],
+        up[1],
+    ])
+
+    abscY = bv + np.array([
+        Vf[0] + up3[0],
+        Vf[0] + up3[1],
+        Vf[1] + up3[0],
+        Vf[1] + up3[1],
+    ])
+
+    return wght, abscX, abscY
+
+
 dt = 0.0005 
 delt = 80
 tstop = 16000
@@ -20,10 +108,70 @@ Mv_moc = []
 Rad_mc = np.zeros((Nsamp, 4))
 Rdot_mc = np.zeros((Nsamp, 4))
 
+momin = np.zeros(6)
+node_locs = np.zeros((4, 2))
+
 for i in range(tstart, tstop, delt):
 	data_qbmm = np.loadtxt("D/prim.6.00." + str(i).zfill(6) + ".dat")[1,1]
 	Rad_qbmm.append(data_qbmm)
 	T_qbmm.append(count*delt*dt)
+
+	if(count == 32):
+		momin[0] = 1.0 
+		momin[1] = data_qbmm
+		momin[2] = np.loadtxt("D/prim.7.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[3] = np.loadtxt("D/prim.8.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[4] = np.loadtxt("D/prim.9.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[5] = np.loadtxt("D/prim.10.00." + str(i).zfill(6) + ".dat")[1,1]
+
+		wght, abscX, abscY = compute_2d_quadrature(momin)
+		node_locs[:, 0] = abscX 
+		node_locs[:, 1] = abscY 
+
+		np.savetxt("Node1.csv", node_locs, delimiter = ',')
+
+	if(count == 62):
+		momin[0] = 1.0 
+		momin[1] = data_qbmm
+		momin[2] = np.loadtxt("D/prim.7.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[3] = np.loadtxt("D/prim.8.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[4] = np.loadtxt("D/prim.9.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[5] = np.loadtxt("D/prim.10.00." + str(i).zfill(6) + ".dat")[1,1]
+
+		wght, abscX, abscY = compute_2d_quadrature(momin)
+		node_locs[:, 0] = abscX 
+		node_locs[:, 1] = abscY 
+
+		np.savetxt("Node2.csv", node_locs, delimiter = ',')
+
+	if(count == 92):
+		momin[0] = 1.0 
+		momin[1] = data_qbmm
+		momin[2] = np.loadtxt("D/prim.7.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[3] = np.loadtxt("D/prim.8.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[4] = np.loadtxt("D/prim.9.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[5] = np.loadtxt("D/prim.10.00." + str(i).zfill(6) + ".dat")[1,1]
+
+		wght, abscX, abscY = compute_2d_quadrature(momin)
+		node_locs[:, 0] = abscX 
+		node_locs[:, 1] = abscY 
+
+		np.savetxt("Node3.csv", node_locs, delimiter = ',')
+
+	if(count == 162):
+		momin[0] = 1.0 
+		momin[1] = data_qbmm
+		momin[2] = np.loadtxt("D/prim.7.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[3] = np.loadtxt("D/prim.8.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[4] = np.loadtxt("D/prim.9.00." + str(i).zfill(6) + ".dat")[1,1]
+		momin[5] = np.loadtxt("D/prim.10.00." + str(i).zfill(6) + ".dat")[1,1]
+
+		wght, abscX, abscY = compute_2d_quadrature(momin)
+		node_locs[:, 0] = abscX 
+		node_locs[:, 1] = abscY 
+
+		np.savetxt("Node4.csv", node_locs, delimiter = ',')
+
 
 	data_qbmm = 0.0
 	for q in range(1, 5, 1):
